15286b8d16931d18bf7c9b088e85d3fb665f67cb
bobbyjoe91 <bobby.cool00763@gmail.com>
2019-07-12 12:03:22 +0700

    Test successfull

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from dashboard.models import Status
 
 # Create your views here.
+def dummy(request):
+    port_status = Status.objects.order_by('ip')
+    var = {'port_status': port_status}
+    return render(request, 'dummy.html', context=var)
