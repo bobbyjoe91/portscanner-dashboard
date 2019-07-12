4d73cf7d5f1e6e1db6907ef66fdb166b56100446
nadyafarchanaf <nadya_farchana@apps.ipb.ac.id>
2019-07-12 15:03:20 +0700

    udah bisa

--- portscanner_dashboard/settings.py
+++ portscanner_dashboard/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 STATICFILES_DIRS = [
     STATIC_DIR
 ]
